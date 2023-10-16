cda6e7c8434ab1985e6a25991cc201010385f36b
Istvan Bereczki <42313348+brczk@users.noreply.github.com>
2023-10-16 16:35:56 +0200

    Post implementation.

---
+++ BlogApp.Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Models
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PostId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string PostTitle { get; set; }
+        [Required]
+        [StringLength(240)]
+        public string PostBody { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+}
