d9587c2c33b94903a46f0b6e862d8d791a6e1c5a
Istvan Bereczki <42313348+brczk@users.noreply.github.com>
2023-10-16 19:20:45 +0200

    Repo tester init.

---
+++ BlogApp.Test/BlogRepoTester.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Repository.Database;
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace BlogApp.Test
+{
+    [TestFixture]
+    public class BlogRepoTester
+    {
+        [Test]
+        public void CreationTest()
+        {
+            BlogDbContext ctx = new BlogDbContext();
+        }
+    }
+}
