b74c6ce35c00737df7c2843f489a709ba7fea984
Istvan Bereczki <42313348+brczk@users.noreply.github.com>
2023-10-16 16:35:15 +0200

    Comment impelementation.

---
+++ BlogApp.Models/Comment.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApp.Models
+{
+    public class Comment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CommentId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(240)]
+        public string CommentBody { get; set; }
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
+    }
+}
