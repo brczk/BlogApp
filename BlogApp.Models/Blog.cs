5bd81d8e43c745c4851772d8d080e62ce305ad48
Istvan Bereczki <42313348+brczk@users.noreply.github.com>
2023-10-16 16:18:22 +0200

    Blog class implementation.

---
+++ BlogApp.Models/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogApp.Models
+{
+    public class Blog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BlogId { get; set; }
+        [Required]
+        [StringLength(240)]
+        public string BlogName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string URL { get; set;}
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
--- BlogApp.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BlogApp.Models
-{
-    public class Class1
-    {
-    }
-}
