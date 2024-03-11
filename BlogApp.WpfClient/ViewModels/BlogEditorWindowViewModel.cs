2047b922cb5a41dc6ba588ba1c3b4bf22ea49200
Istvan Bereczki <42313348+brczk@users.noreply.github.com>
2024-03-11 17:08:53 +0100

    BlogEditor crud

--- BlogApp.WpfClient/ViewModels/BlogEditorWindowViewModel.cs
+++ BlogApp.WpfClient/ViewModels/BlogEditorWindowViewModel.cs
@@ -1,22 +1,80 @@
 ﻿using BlogApp.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace BlogApp.WpfClient.ViewModels
 {
-    public class BlogEditorWindowViewModel
+    public class BlogEditorWindowViewModel : ObservableRecipient
     {
         public RestCollection<Blog> Blogs
         {
             get; set;
         }
 
+        public ICommand CreateBlogCommand
+        {
+            get; set;
+        }
+
+        public ICommand DeleteBlogCommand
+        {
+            get; set;
+        }
+
+        public ICommand UpdateBlogCommand
+        {
+            get; set;
+        }
+
+        private Blog selectedBlog;
+
+        public Blog SelectedBlog
+        {
+            get { return selectedBlog; }
+            set {
+                if (value != null)
+                {
+                    selectedBlog = new Blog()
+                    {
+                        Id = value.Id,
+                        BlogName = value.BlogName,
+                        Posts = value.Posts
+                    };
+                    OnPropertyChanged();
+                    (DeleteBlogCommand as RelayCommand).NotifyCanExecuteChanged();
+                }  
+            }
+        }
+
+
         public BlogEditorWindowViewModel()
         {
             Blogs = new RestCollection<Blog>("http://localhost:5828/", "blog");
+            CreateBlogCommand = new RelayCommand(() =>
+            {
+                Blogs.Add(new Blog()
+                {
+                    BlogName = SelectedBlog.BlogName
+                });
+            });
+
+            DeleteBlogCommand = new RelayCommand(() =>
+            {
+                Blogs.Delete(SelectedBlog.Id);
+            }, () => SelectedBlog != null);
+
+            UpdateBlogCommand = new RelayCommand(() =>
+            {
+                Blogs.Update(SelectedBlog);
+            });
+            SelectedBlog = new Blog();
         }
     }
 }
